e90a99d02d8157b96a205fd1720ae0331432bcc2
Soumitro Khisa <hospanataniya@gmail.com>
2025-04-02 10:56:56 +0600

    serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -6,7 +6,6 @@ self.addEventListener('install', event => {
               '/index.html',
               '/styles.css',
               '/script.js',
-              '/offline.html',
               '/manifest.json'
           ]);
       })
@@ -16,8 +15,7 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
       caches.match(event.request).then(response => {
-          return response || fetch(event.request)
-          .then(response => response || caches.match("/offline.html"));
+          return response || fetch(event.request);
       })
   );
 });
